9702315e105e3ae1242526fc0b17e14e184ecc68
Ahmed Sammari <sammari.ahmed@gmail.com>
2017-06-20 04:39:48 +0100

    fixed profil (eleve and enseignant)

--- src/AppBundle/Form/EleveType.php
+++ src/AppBundle/Form/EleveType.php
@@ -17,7 +17,14 @@ class EleveType extends AbstractType
         $builder
             ->add('address')
             ->add('email')
-            ->add('username');
+            ->add('username')
+            ->add('plainPassword', 'repeated', array(
+                'type' => 'password',
+                'options' => array('translation_domain' => 'FOSUserBundle'),
+                'first_options' => array('label' => 'form.password'),
+                'second_options' => array('label' => 'form.password_confirmation'),
+                'invalid_message' => 'fos_user.password.mismatch',
+            ));
     }
     
     /**
